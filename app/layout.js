e54fc9228eee9b325040295d29bf79ccea9eba57
axeemgohar <axeemgohar@gmail.com>
2025-12-06 17:34:31 +0500

    Header ad optimized

--- app/layout.js
+++ app/layout.js
@@ -43,7 +43,7 @@ export default function RootLayout({ children }) {
         <Toaster position="top-right" />
         <GoogleTagManager gtmId="GTM-MPPN39V" />
         <EzoicScripts />
-        <div className="h-28 text-center">
+        <div className="max-h-28 text-center max-w-5xl mx-auto overflow-hidden">
           <div id="ezoic-pub-ad-placeholder-123"></div>
         </div>
 
