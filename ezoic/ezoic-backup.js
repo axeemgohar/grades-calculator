97c33d933adb7e23c7ae3d15ac73df01ae5868c5
axeemgohar <axeemgohar@gmail.com>
2025-12-07 18:56:51 +0500

    Ezoic integration migrated to zaraz

--- ezoic/ezoic-backup.js
+++ ezoic/ezoic-backup.js
@@ -20,7 +20,7 @@ const EzoicScripts = () => {
         console.error('Error pushing ads: ', err);
         clearInterval(intervalId);
       }
-    }, 100);
+    }, 500);
 
     // Clear interval on component unmount
     return () => clearInterval(intervalId);
@@ -28,28 +28,41 @@ const EzoicScripts = () => {
 
   return (
     <>
+      <link rel="preconnect" href="https://www.ezojs.com" />
+      <link rel="dns-prefetch" href="https://www.ezojs.com" />
+      <link rel="preconnect" href="https://the.gatekeeperconsent.com" />
+      <link rel="dns-prefetch" href="https://the.gatekeeperconsent.com" />
+      <link rel="preconnect" href="https://cmp.gatekeeperconsent.com" />
+      <link rel="dns-prefetch" href="https://cmp.gatekeeperconsent.com" />
+
       {/* Ezoic Privacy Scripts */}
       <Script
         id="ezoic-privacy"
         src="https://the.gatekeeperconsent.com/cmp.min.js"
         data-cfasync="false"
-        strategy="lazyOnload"
+        strategy="afterInteractive"
       />
       <Script
         id="ezoic-privacy-cmp"
         src="https://cmp.gatekeeperconsent.com/min.js"
         data-cfasync="false"
-        strategy="lazyOnload"
+        strategy="afterInteractive"
       />
 
       {/* Ezoic Main Script */}
       <Script
-        id="ezoic-header"
-        async
         src="//www.ezojs.com/ezoic/sa.min.js"
+        id="ezoic-header"
+        strategy="afterInteractive"
         onLoad={() => {
-          window.ezstandalone = window.ezstandalone || {};
-          window.ezstandalone.cmd = window.ezstandalone.cmd || [];
+          // Initialize ezstandalone object
+          if (typeof window !== 'undefined') {
+            window.ezstandalone = window.ezstandalone || {};
+            window.ezstandalone.cmd = window.ezstandalone.cmd || [];
+          }
+        }}
+        onError={(e) => {
+          console.error('Failed to load Ezoic script:', e);
         }}
       />
     </>
